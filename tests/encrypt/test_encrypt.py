ca99840d490cdd838713c41411a0251bf8966d26
deborafantinif <deboraferfan@gmail.com>
2022-11-28 08:51:23 -0300

    FEAT[req 2]: testar função

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
-
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 def test_encrypt_message():
-    pass
+    message = encrypt_message("AABBCC", 3)
+    assert "BAA_CCB" == message
+
+    message = encrypt_message("ABBCCA", 4)
+    assert "AC_CBBA" == message
+
+    message = encrypt_message("AABBCC", -1)
+    assert "CCBBAA" == message
+
+    with pytest.raises(TypeError):
+        encrypt_message("AABBCC", '4')
+
+    with pytest.raises(TypeError):
+        encrypt_message(59, 2)
