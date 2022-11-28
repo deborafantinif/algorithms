55ae39d57fd69a4ca31d60035e137301eb2825d2
deborafantinif <deboraferfan@gmail.com>
2022-11-28 08:28:30 -0300

    FEAT[req 01]: verificar quantas vezes determinado horário se repete

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) is not int or permanence_period is None:
+        return None
+    times = 0
+    for time in permanence_period:
+        if type(time[0]) is not int or type(time[1]) is not int:
+            return None
+        if time[0] <= target_time and time[1] >= target_time:
+            times += 1
+    return times
