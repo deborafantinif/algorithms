3baa10de2c8cdd36c7da1fba1a6ef72b04c03e15
deborafantinif <deboraferfan@gmail.com>
2022-11-28 10:04:02 -0300

    FEAT[req 3]: palindromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,4 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if word == '':
         return False
-    
+    if low_index == high_index:
+        return True
+    if low_index >= high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    next_low_index = low_index + 1
+    next_high_index = high_index + 1
+    return is_palindrome_recursive(word, next_low_index, next_high_index)
+
