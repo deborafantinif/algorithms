cc80478e316f43cbfc0c94505e4dcdf2ac4c85ad
deborafantinif <deboraferfan@gmail.com>
2022-11-30 15:37:48 -0300

    FIX[req 3]: troca de sinal + por -

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,26 @@
+def sorted_word(word):
+    n = len(word)
+
+    for index in range(n - 1):
+        min_element_index = index
+
+        for search_index in range(index + 1, n):
+            if word[search_index] < word[min_element_index]:
+                min_element_index = search_index
+
+        current_element = word[index]
+        word[index] = word[min_element_index]
+        word[min_element_index] = current_element
+
+    return word
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_formated = sorted_word(first_string.lower())
+    second_formated = sorted_word(second_string.lower())
+
+    if len(first_string) != len(second_string):
+        return (first_formated, second_formated, False)
+    else:
+        return (first_formated, second_formated, True)
+
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word == '':
+    if word == '' or word[low_index] != word[high_index]:
         return False
-    if low_index == high_index:
-        return True
     if low_index >= high_index:
         return True
-    if word[low_index] != word[high_index]:
-        return False
     next_low_index = low_index + 1
-    next_high_index = high_index + 1
+    next_high_index = high_index - 1
     return is_palindrome_recursive(word, next_low_index, next_high_index)
 
