2e5304f3320a72b39dedcc21f88fe9d89d14ee09
deborafantinif <deboraferfan@gmail.com>
2022-11-30 16:41:30 -0300

    FEAT[req 5]: verifivação da entrada

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,3 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(nums) is not list or len(nums) < 2 or type(nums[0]) is not int:
+        return False
