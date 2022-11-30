8cb9c7fdb76067e83c9f8bcc6b0c36145bffe334
deborafantinif <deboraferfan@gmail.com>
2022-11-30 16:33:02 -0300

    FEAT[req 6]: palindromo iterativo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,5 +1,14 @@
-# def is_palindrome_iterative(word):
-#     n = len(word)
-#     initial = 0
-#     while n >= 0:
-#         pass
+def is_palindrome_iterative(word):
+    if word == '':
+        return False
+
+    n = len(word) - 1
+    initial = 0
+
+    while n > initial:
+        if word[initial] != word[n]:
+            return False
+        n -= 1
+        initial += 1
+
+    return True
