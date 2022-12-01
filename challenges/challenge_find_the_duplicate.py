c83abd469af6e67363c8566c686c9616c1e94634
deborafantinif <deboraferfan@gmail.com>
2022-12-01 14:56:03 -0300

    FEAT[req 5]: verificação dos numeros que não de repitem

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
     if type(nums) is not list or len(nums) < 2 or type(nums[0]) is not int:
         return False
+    for number in nums:
+        if number < 0:
+            return False
+        for next_number in range(nums.index(number) + 1, len(nums)):
+            if number == nums[next_number]:
+                return True
+    return False
+
+
+print(find_duplicate([1,2]))
