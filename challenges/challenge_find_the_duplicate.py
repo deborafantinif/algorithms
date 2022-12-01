2a538b86da395812b10cb44bd85f545e40f9a46d
deborafantinif <deboraferfan@gmail.com>
2022-12-01 15:38:41 -0300

    FEAT[req 5]: lógica completa para encontrar duplicidade

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,7 +1,25 @@
+def count_numbers(nums, number, report_numbers):
+    for next_number in range(nums.index(number) + 1, len(nums)):
+        if number == nums[next_number]:
+            report_numbers[number] += 1
+
+
+def find_max_repeat(dict_n):
+    maxi = [0, 0]
+    for key, value in dict_n.items():
+        if value > maxi[1]:
+            maxi = [key, value]
+    return maxi
+
+
 def find_duplicate(nums):
+    report_numbers = {}
     if type(nums) is not list or len(nums) < 2 or type(nums[0]) is not int:
         return False
     for number in nums:
         if number < 0:
             return False
-    return False
+        report_numbers[number] = 1
+        count_numbers(nums, number, report_numbers)
+    maxi_number = find_max_repeat(report_numbers)
+    return maxi_number[0] if maxi_number[1] != 1 else False
